fb8c72dce0ba9df945fc145a88907f615feb3b88
Aliyu Zakari <aliyubzakari@gmail.com>
2017-04-05 19:07:54 -0400

    Updates

--- Personal_Projects/Web_Development/practise/prac_7/prac_7.js
+++ Personal_Projects/Web_Development/practise/prac_7/prac_7.js
@@ -2,3 +2,17 @@
  
  */
 
+//finds element by id
+//document.getElementById(id) ;
+
+//finds elements by class name
+//document.getElementsByClassName(name) ;
+
+//finds elements by tag name
+//document.getElementsByTagName(name);
+
+var elem = document.getElementById("a");
+var leo = document.getElementById("b");
+elem.innerHTML = "Welcome to the page";
+
+leo.innerHTML = "Quick attack";
